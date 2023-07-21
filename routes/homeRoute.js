b4c9e9663443ec52ea0a7b2f16458f1a1db8b4e4
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2023-07-21 23:27:24 +0200

    Update homeRoute.js

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,11 +1,9 @@
 //routes/homeRoute.js
-const { logger } = require('../helper/saveSubscription');
-
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  logger.info('GET request received');
+  console.log('GET request received');
   res.sendStatus(200);
 });
 
