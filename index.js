bd3426f1214b8219e1e024a6a0d5f7340581b0b1
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2023-07-25 21:40:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const responseTime = require('response-time');
 const compression = require('compression'); // Import the compression middleware
-
+const pm2 = require('pm2');
 require('dotenv').config();
 const webApp = express();
 const PORT = process.env.PORT || 3000;
